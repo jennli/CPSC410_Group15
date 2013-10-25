2b570a44f2288788025852300a9805210227e751
jennli <ldnjennifer@hotmail.com>
2013-10-25 14:54:53 -0700

    Test file

---
+++ src/Test.java
@@ -0,0 +1,37 @@
+import java.io.File;
+import java.util.Collection;
+
+import com.thoughtworks.qdox.JavaProjectBuilder;
+import com.thoughtworks.qdox.model.JavaClass;
+import com.thoughtworks.qdox.model.JavaField;
+
+public class Test {
+
+    
+    
+    public Test() {
+        // TODO Auto-generated constructor stub
+    }
+
+    public static void main(String[] args) {
+        // create new JavaProjectBuilder
+        JavaProjectBuilder builder = new JavaProjectBuilder();
+        builder.addSourceTree(new File("org")); // path to JHotDraw
+        Collection<JavaClass> classes = builder.getClasses();
+        for (JavaClass c : classes) {
+            System.out.println("\nClass: " + c.getName());
+            Collection<JavaClass> derived = c.getDerivedClasses();
+            for (JavaClass d : derived) {
+                System.out.println("\t\tSubclass: " + d);
+                Collection<JavaField> fields = d.getFields();
+                
+                for (JavaField f : fields) {
+                    if (f.getType().getFullyQualifiedName().equals("java.util.List")) {
+                        System.out.println("\t\t\tField of type List: " + f);
+                    }
+                }
+            }
+        }
+    }
+
+}
