81ddd7f5584395c0d2e041e1c4d0ffccc4ed538a
jennli <ldnjennifer@hotmail.com>
2013-10-28 15:38:14 -0700

    modify the return statement of the patternMatch method in SingletonPatternMatcher

--- src/SingletonPatternMatcher.java
+++ src/SingletonPatternMatcher.java
@@ -7,7 +7,7 @@ import com.thoughtworks.qdox.model.JavaMethod;
 
 public class SingletonPatternMatcher extends AbstractPatternMatcher {
 
-	private Collection<JavaClass> singletons = new LinkedList<JavaClass>();
+	private Collection<Collection<JavaClass>> singletons = new LinkedList<Collection<JavaClass>>();
 	private JavaProjectBuilder jpb;
 
 	public SingletonPatternMatcher(JavaProjectBuilder pb) {
@@ -19,6 +19,7 @@ public class SingletonPatternMatcher extends AbstractPatternMatcher {
 			JavaProjectBuilder builder) {
 		// TODO Auto-generated method stub
 		Collection<JavaClass> classes = jpb.getClasses();
+		Collection<JavaClass> oneMatchInstance = new LinkedList<JavaClass>();
 		for (JavaClass c : classes) {
 			System.out.println("\nClass being analyzed: " + c.getName());
 			Collection<JavaMethod> methods = c.getMethods();
@@ -27,11 +28,13 @@ public class SingletonPatternMatcher extends AbstractPatternMatcher {
 						.println("\t\tMethod being analyzed: " + jm.getName());
 				if (jm.getName().equals("getInstance")) {
 					System.out.println(c.getName() + " has a possible match");
-					singletons.add(c);
+					// singletons.add(c);
+					oneMatchInstance.add(c);
+					singletons.add(oneMatchInstance);
 				}
 			}
 		}
-		return null;
-	}
 
+		return singletons;
+	}
 }
