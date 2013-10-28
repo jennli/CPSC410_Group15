fb8817fcf2e461690ca6ba0c981f0876d63aca41
toxakktl <toxa_kktl@mail.ru>
2013-10-27 19:38:45 -0700

    Rough Algorithm for searching Singleton Design pattern. According to my algorithm , there is no Singleton Design pattern in JHotDraw. It might contain some errors. But i tested it with other methods and public constructors, seems like it works

---
+++ src/PatternSearcher.java
@@ -0,0 +1,50 @@
+import java.io.File;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import com.thoughtworks.qdox.JavaProjectBuilder;
+import com.thoughtworks.qdox.model.JavaClass;
+import com.thoughtworks.qdox.model.JavaConstructor;
+import com.thoughtworks.qdox.model.JavaMethod;
+
+
+public class PatternSearcher {
+
+	Set<String> classNames = new HashSet<String>();
+
+	public PatternSearcher(){
+		
+	}
+	
+	public void SingletonSearch(){
+		JavaProjectBuilder builder = new JavaProjectBuilder();
+		builder.addSourceTree(new File("/Users/toxa/Desktop/JHotD/src/org")); // path to JHotDraw
+		Collection<JavaClass> classes = builder.getClasses();
+		for (JavaClass c: classes){
+			List<JavaMethod> methods = c.getMethods();
+			for (JavaMethod m: methods){
+				//System.out.println("Method name is: " + m.getName());
+				if (m.getName().equals("getInstance")){
+					Collection<JavaConstructor> jCon= c.getConstructors();
+					for (JavaConstructor jc : jCon){
+						List<String> jm = jc.getModifiers();
+						for (String s : jm){
+							if (s.equals("private")){
+								classNames.add(c.getName());
+							}
+						}
+					}
+				}
+			}
+		}
+		if (classNames.isEmpty()){
+			System.out.println("No class is involved in Singleton design pattern");
+		}else
+			
+		for(String l : classNames){
+			System.out.println("The class name that is involved in Singleton design patter: " + l);
+		}
+	}
+}
--- src/Test.java
+++ src/Test.java
@@ -14,24 +14,26 @@ public class Test {
     }
 
     public static void main(String[] args) {
+    	PatternSearcher  ps = new PatternSearcher();
+    	ps.SingletonSearch();
         // create new JavaProjectBuilder
-        JavaProjectBuilder builder = new JavaProjectBuilder();
-        builder.addSourceTree(new File("org")); // path to JHotDraw
-        Collection<JavaClass> classes = builder.getClasses();
-        for (JavaClass c : classes) {
-            System.out.println("\nClass: " + c.getName());
-            Collection<JavaClass> derived = c.getDerivedClasses();
-            for (JavaClass d : derived) {
-                System.out.println("\t\tSubclass: " + d);
-                Collection<JavaField> fields = d.getFields();
-                
-                for (JavaField f : fields) {
-                    if (f.getType().getFullyQualifiedName().equals("java.util.List")) {
-                        System.out.println("\t\t\tField of type List: " + f);
-                    }
-                }
-            }
-        }
+//        JavaProjectBuilder builder = new JavaProjectBuilder();
+//        builder.addSourceTree(new File("/Users/toxa/Desktop/JHotD/src/org")); // path to JHotDraw
+//        Collection<JavaClass> classes = builder.getClasses();
+//        for (JavaClass c : classes) {
+//            System.out.println("\nClass: " + c.getName());
+//            Collection<JavaClass> derived = c.getDerivedClasses();
+//            for (JavaClass d : derived) {
+//                System.out.println("\t\tSubclass: " + d);
+//                Collection<JavaField> fields = d.getFields();
+//                
+//                for (JavaField f : fields) {
+//                    if (f.getType().getFullyQualifiedName().equals("java.util.List")) {
+//                        System.out.println("\t\t\tField of type List: " + f);
+//                    }
+//                }
+//            }
+//        }
     }
 
 }
