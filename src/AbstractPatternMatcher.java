7b939af84bfe4f0338a27789eb7fc9dc4ccedf72
Alex <Alex@128.189.221.25>
2013-10-28 14:52:00 -0700

    Changed patternMatch method to accept a JavaProjectBuilder object

--- src/AbstractPatternMatcher.java
+++ src/AbstractPatternMatcher.java
@@ -1,9 +1,10 @@
 import java.util.Collection;
 
+import com.thoughtworks.qdox.JavaProjectBuilder;
 import com.thoughtworks.qdox.model.JavaClass;
 
 public abstract class AbstractPatternMatcher {
 	
-	public abstract Collection<Collection<JavaClass>> patternMatch();
+	public abstract Collection<Collection<JavaClass>> patternMatch(JavaProjectBuilder builder);
 	
 }
