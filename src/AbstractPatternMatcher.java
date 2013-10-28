e6d887db0f4408c0ce92eaaffd514fa20be8407b
jennli <ldnjennifer@hotmail.com>
2013-10-28 14:23:31 -0700

    added AbstractPatternMatcher

---
+++ src/AbstractPatternMatcher.java
@@ -0,0 +1,10 @@
+import java.util.Collection;
+
+import com.thoughtworks.qdox.model.JavaClass;
+
+public abstract class AbstractPatternMatcher {
+
+	
+	public abstract void patternMatch();
+	public Collection<JavaClass> returnMatchedClasses;
+}
