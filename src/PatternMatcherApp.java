cb954331dde2d7a8fc80c78ef1c8e55275cb1a67
jennli <ldnjennifer@hotmail.com>
2013-10-28 11:46:18 -0700

    Renamed PatternAnalyzer to PatternMatcherApp

---
+++ src/PatternMatcherApp.java
@@ -0,0 +1,15 @@
+import java.io.File;
+
+import com.thoughtworks.qdox.JavaProjectBuilder;
+
+public class PatternMatcherApp {
+
+	public static void main(String[] args) {
+		JavaProjectBuilder builder = new JavaProjectBuilder();
+		builder.addSourceTree(new File("org")); // path to JHotDraw
+		SingletonPatternMatcher spf = new SingletonPatternMatcher(builder);
+		spf.singletonSearch();
+		System.out.println("Number of singleton pattern used in JHotDraw: "
+				+ spf.getSingletons().size());
+	}
+}
