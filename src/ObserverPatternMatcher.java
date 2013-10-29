11c5c5dc197bf64bb3463fdad2fc9e1cf50e4fbb
jennli <ldnjennifer@hotmail.com>
2013-10-29 14:50:47 -0700

    Add ObserverPatternMatcher.java #5

---
+++ src/ObserverPatternMatcher.java
@@ -0,0 +1,16 @@
+import java.util.Collection;
+
+import com.thoughtworks.qdox.JavaProjectBuilder;
+import com.thoughtworks.qdox.model.JavaClass;
+
+
+public class ObserverPatternMatcher extends AbstractPatternMatcher {
+
+	@Override
+	public Collection<Collection<JavaClass>> patternMatch(
+			JavaProjectBuilder builder) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
